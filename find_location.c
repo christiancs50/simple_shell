0f18433f83bb94898528ae107fa622d62455a2be
“christiancs50” <christian.gaoso@gmail.com>
2023-12-18 14:02:22 +0000

    update findlocation

--- find_location.c
+++ find_location.c
@@ -1,75 +1,50 @@
 #include "shell.h"
-char *allocate_command_path(const char *path, const char *command);
-/**
- * allocate_command_path - function that returns the path of a command
- * @command: name of the command
- * @path: path to the command
- * Return: the path of the command or NULL if not found
- */
-char *allocate_command_path(const char *path, const char *command)
-{
-	char *path_copy = strdup(path);
-	char *path_token;
-	char *file_path = NULL;
-	size_t max_path_length = PATH_MAX;
-	struct stat buffer;
-
-	if (!path_copy)
-	{
-		perror("strdup");
-		free(path_copy);
-		return (NULL);
-	}
-
-	path_token = strtok(path_copy, ":");
-
-	while (path_token != NULL)
-	{ /* Allocate memory for the complete path */
-		file_path = malloc(max_path_length + 1);
-
-		if (!file_path)
-		{
-			perror("malloc");
-			free(path_copy);
-			return (NULL);
-		}
-
-		snprintf(file_path, max_path_length, "%s/%s",
-				path_token, command);
-
-		/* Check if this file path actually exists */
-		if (stat(file_path, &buffer) == 0)
-		{
-			free(path_copy);
-			return (file_path);
-		}
-		free(file_path);
-		path_token = strtok(NULL, ":");
-	}
-
-	/* Free allocated memory before returning NULL */
-	free(path_copy);
-
-	return (NULL);
-
-}
 
 /**
  * get_location - function that returns the path of a command
  * @command: name of the command
  * Return: the path of the command or NULL if not found
  */
-
 char *get_location(const char *command)
 {
-	char *command_path;
-	char *path = getenv("PATH");
+	char *path, *path_copy, *path_token, *file_path;
+	size_t max_path_length;
+	struct stat buffer;
 
+	path = getenv("PATH");
 	if (!path)
+		return (NULL);
+	path_copy = strdup(path);
+	if (!path_copy)
 	{
+		perror("strdup");
 		return (NULL);
 	}
-
-	command_path = allocate_command_path(path, command);
-	return (command_path);
+	path_token = strtok(path_copy, ":");
+	max_path_length = PATH_MAX;	/* PATH_MAX is a constant defined by the system */
+	file_path = malloc(max_path_length + 1);	/* +1 for null terminator*/
+	if (!file_path)
+	{
+		perror("malloc");
+		free(path_copy);
+		return (NULL);
+	}
+	while (path_token != NULL)
+	{ /* Build the path for the command */
+		snprintf(file_path, max_path_length, "%s/%s", path_token, command);
+		if (stat(file_path, &buffer) == 0)
+		{
+			free(path_copy);
+			return (file_path);
+		}
+		path_token = strtok(NULL, ":");
+	}	/* Check if the command itself is a file path that exists */
+	if (stat(command, &buffer) == 0)
+	{
+		free(path_copy);
+		return (strdup(command));
+	}	/* Free allocated memory before returning NULL */
+	free(file_path);
+	free(path_copy);
+	return (NULL);
 }
